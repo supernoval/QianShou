1acc2244633b819193eccf0f5902adfd16ae9b6a
tabttoo1235 <tabttoo1235@users.noreply.github.com>
2015-08-09 13:45:40 +0800

    liu

---
+++ QianShouBang/NearPeopleCell.h
@@ -0,0 +1,13 @@
+//
+//  NearPeopleCell.h
+//  QianShouBang
+//
+//  Created by Leo on 15/8/9.
+//  Copyright (c) 2015年 zhuhaikun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NearPeopleCell : UITableViewCell
+
+@end
