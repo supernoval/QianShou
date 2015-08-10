bcbc80f1a54b0832d5d2e019a104e001de17f8ca
bimofan <youlikethisgame@gmail.com>
2015-08-10 17:00:44 +0800

    zhu

---
+++ QianShouBang/QianShouBang/Yell/YellTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  YellTableViewController.h
+//  QianShouBang
+//
+//  Created by Haikun Zhu on 15/8/10.
+//  Copyright (c) 2015年 zhuhaikun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YellTableViewController : UITableViewController
+
+@end
